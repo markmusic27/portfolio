42d1dbf032784b22953ff196b0c0e86aae98c120
Mark Music <markmusic999@gmail.com>
2021-01-26 16:18:57 -0600

    ft[programming] => 🐛 Add HeaderWithIcon to Programming Block

--- lib/widgets/contentBox/local_widgets/programming.block.dart
+++ lib/widgets/contentBox/local_widgets/programming.block.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:portfolio/widgets/headerWithIcon/headerWithIcon.ui.dart';
 
 class Programming extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(top: 10),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          HeaderWithIcon(text: "Programming", emoji: Emoji.keyboard),
+        ],
+      ),
+    );
   }
 }
